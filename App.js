71df8be5a2683a75c2b6affcc598b7aa356977f7
Geyser Ramírez <geyser.grs@gmail.com>
2021-03-02 16:37:02 -0600

    prueba

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.h1}>Welcome to</Text>
+      <Text style={styles.h1}>Bowhead's</Text>
+      <Text>Wellness Logger</Text>
+      <Button
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+        title="START"
+        color="#1dacc1"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  h1: {
+    fontSize: 30
+  },
+  h2: {
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
