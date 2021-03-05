6c6049ce99c870aca467b98ccbfe6453ed7dcba1
Geyser Ramírez <geyser.grs@gmail.com>
2021-03-04 22:03:44 -0600

    sombra a los tabs

--- screens/ScreenLogger.js
+++ screens/ScreenLogger.js
@@ -10,29 +10,31 @@ export default function ScreenLogger() {
   return (
     <Tab.Navigator
     screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+      tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'New Logger') {
-              iconName = 'home';
-            } else if (route.name === 'Logs') {
-              iconName = 'list';
-            }
-            return <Ionicons name={iconName} size={30} color={color} />;
-        },
+          if (route.name === 'New Logger') {
+            iconName = 'home';
+          } else if (route.name === 'Logs') {
+            iconName = 'list';
+          }
+          return <Ionicons name={iconName} size={30} color={color} />;
+      },
     })}
         tabBarOptions={{
         activeTintColor: '#1dacc1',
         inactiveTintColor: 'gray',
         showLabel: false,
         tabStyle: {
-            minHeight: 80,
-            shadowOffset: {
-                width: 0,
-                height: 0
-            },
-            shadowOpacity: .15,
-            shadowRadius: 6,
+          minHeight: 80,
+        },
+        style: {
+          shadowOffset: {
+            width: 0,
+            height: -5
+          },
+          shadowOpacity: .2,
+          shadowRadius: 6,
         }
     }}
     >
